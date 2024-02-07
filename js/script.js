93f015de7fd796e1bd95db5eecfd5019e8521fbf
lluisllo <lluisllodra@gmail.com>
2024-02-07 17:06:21 +0100

    Generador 100% funcional, no customizable (16 dígitos)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-let fullRandomString = "";
+// Arrays ASCII
+// https://fastbitlab.com/wp-content/uploads/2022/05/Figure-1-15-1200x705.png
+const mayusculas = asciiToArray(65, 90);
+const minusculas = asciiToArray(97, 122);
+const numeros = asciiToArray(48, 57);
+const simbolos = asciiToArray(33, 47).concat(
+  // --> ! " # $ % & ' ( ) * + ´ - . /
+  asciiToArray(58, 64), // --> : ; < = > ? @
+  asciiToArray(91, 96), // --> [ / ] ^ _ `
+  asciiToArray(123, 126) // --> { | } ~
+);
+
+// Elegir posición de array aleatoria
+function getRandomChar(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+
+  return array[randomIndex];
+}
 
 // Valor ASCII a array
 function asciiToArray(start, end) {
@@ -11,51 +28,77 @@ function asciiToArray(start, end) {
   return array;
 }
 
-// Elegir posición de array aleatoria
-function getRandomChar(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-
-  return array[randomIndex];
-}
+let randomString = "";
+let fullRandomString = "";
 
 // Generar contraseña
 function nuevaContraseña() {
-  // Arrays ASCII
-  // https://fastbitlab.com/wp-content/uploads/2022/05/Figure-1-15-1200x705.png
-  const mayusculas = asciiToArray(65, 90);
-  const minusculas = asciiToArray(97, 122);
-  const numeros = asciiToArray(48, 57);
-  const simbolos = asciiToArray(33, 47).concat(
-    // ! " # $ % & ' ( ) * + ´ - . /
-    asciiToArray(58, 64), // : ; < = > ? @
-    asciiToArray(91, 96), // [ / ] ^ _ `
-    asciiToArray(123, 126) // { | } ~
-  );
-
   let randomString = "";
+  let counts = { 0: 0, 1: 0, 2: 0, 3: 0 };
+  let caracterAnterior = -1;
 
-  const length = 30;
+  const length = 16;
   for (let i = 0; i < length; i++) {
-    const randomCategory = Math.floor(Math.random() * 4);
+    let randomArray;
 
-    switch (randomCategory) {
+    // Evita dos carácteres seguidos procedentes del mismo array
+    do {
+      randomArray = Math.floor(Math.random() * 4);
+    } while (randomArray === caracterAnterior);
+
+    caracterAnterior = randomArray;
+
+    // Garantiza que haya, al menos, dos carácteres de cada tipo
+    if (
+      i === length - 1 &&
+      !Object.values(counts).every((count) => count >= 2)
+    ) {
+      return nuevaContraseña();
+    }
+
+    let charToAdd;
+    switch (randomArray) {
       case 0:
-        randomString += getRandomChar(mayusculas);
+        charToAdd = getRandomChar(mayusculas);
+        counts[0]++;
         break;
       case 1:
-        randomString += getRandomChar(minusculas);
+        charToAdd = getRandomChar(minusculas);
+        counts[1]++;
         break;
       case 2:
-        randomString += getRandomChar(numeros);
+        charToAdd = getRandomChar(simbolos);
+        counts[2]++;
         break;
       case 3:
-        randomString += getRandomChar(simbolos);
+        charToAdd = getRandomChar(numeros);
+        counts[3]++;
         break;
     }
+
+    randomString += charToAdd;
   }
 
-  fullRandomString = randomString;
+  if (comprovarAdyacencia(randomString)) {
+    randomString;
+  } else {
+    return nuevaContraseña();
+  }
 
+  if (comprovarDuplicidad(randomString)) {
+    randomString;
+  } else {
+    return nuevaContraseña();
+  }
+
+  if (comprovarUpperLower(randomString)) {
+    randomString;
+  } else {
+    return nuevaContraseña();
+  }
+
+  // Ocutar contraseña a partir de X carácteres
+  fullRandomString = randomString;
   const displayedString =
     randomString.length > 15 ? randomString.slice(0, 15) + "..." : randomString;
 
@@ -64,7 +107,50 @@ function nuevaContraseña() {
   document.getElementById("password").textContent = displayedString;
 }
 
-// Copy to clipboard
+// Garantiza que no haya valores ASCII adyacentes entre si
+function comprovarAdyacencia(randomString) {
+  for (let i = 1; i < randomString.length; i++) {
+    const currentCharCode = randomString.charCodeAt(i);
+    const prevCharCode = randomString.charCodeAt(i - 1);
+    if (
+      currentCharCode === prevCharCode ||
+      currentCharCode === prevCharCode + 1 ||
+      currentCharCode === prevCharCode - 1
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Comprueba que no se repitan caracteres
+function comprovarDuplicidad(randomString) {
+  const presente = new Set();
+  for (let i = 0; i < randomString.length; i++) {
+    const charCode = randomString.charCodeAt(i);
+    if (presente.has(charCode)) {
+      return false;
+    }
+    presente.add(charCode);
+  }
+  return true;
+}
+
+// Comprueba que no haya una misma letra en mayúscula y minúscula
+function comprovarUpperLower(randomString) {
+  const seen = new Set();
+  for (let i = 0; i < randomString.length; i++) {
+    const char = randomString[i].toLowerCase();
+    const charCode = char.charCodeAt(0);
+    if (seen.has(charCode)) {
+      return false;
+    }
+    seen.add(charCode);
+  }
+  return true;
+}
+
+// Copiar al portapapeles
 document
   .getElementById("copy-to-clipboard")
   .addEventListener("click", function () {
